25566587f4f893d0663765249c3937e8e8af50c5
Nic Barker <bicnarker@me.com>
2018-05-23 10:32:21 +1000

    Fixed a bug where the create gates modal warning was incorrectly displaying under some circumstances

--- js/containers/homology-modal-container.jsx
+++ js/containers/homology-modal-container.jsx
@@ -11,7 +11,8 @@ const mapStateToProps = (state, ownProps) => {
     const selectedWorkspace = _.find(state.workspaces, w => w.id === state.selectedWorkspaceId) || {}
     const selectedFCSFile = _.find(state.FCSFiles, fcs => fcs.id === selectedWorkspace.selectedFCSFileId) || {}
     const selectedGateTemplate = _.find(state.gateTemplates, gt => gt.id === selectedWorkspace.selectedGateTemplateId) || {}
-    const gateHasChildren = _.find(state.gateTemplateGroups, g => g.parentGateTemplateId === selectedGateTemplate.id) ? true : false
+    const selectedGateTemplateGroup = _.find(state.gateTemplateGroups, g => g.parentGateTemplateId === selectedGateTemplate.id && g.selectedXParameterIndex === state.modals.homology.selectedXParameterIndex && g.selectedYParameterIndex === state.modals.homology.selectedYParameterIndex) 
+    const gateHasChildren = selectedGateTemplateGroup ? true : false
     const selectedSample = _.find(state.samples, s => s.gateTemplateId === selectedGateTemplate.id && s.FCSFileId === selectedFCSFile.id) || {}
 
     return {
